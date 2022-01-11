3b48d5e49a5bafe5a071839a5aa9e95e5487971d
Joshua Samaniego <joshuadsamaniego@gmail.com>
2022-01-11 09:31:30 -0700

    new PR to resolve merge conflicts

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -3,146 +3,117 @@
 pragma solidity ^0.7.6;
 pragma experimental ABIEncoderV2;
 
-import "usingtellor/contracts/UsingTellor.sol";
 import "hardhat/console.sol";
 
 interface Oracle {
-    function getUintVar(bytes32 _data) external view returns (uint256);
-
-    function getNewValueCountbyRequestId(uint256 _requestId)
+    function getReportTimestampByIndex(bytes32 _queryId, uint256 _index)
         external
         view
         returns (uint256);
 
-    function getTimestampbyRequestIDandIndex(uint256 _requestID, uint256 _index)
+    function getValueByTimestamp(bytes32 _queryId, uint256 _timestamp)
+        external
+        view
+        returns (bytes memory);
+
+    //get last values on a request id
+    function getTimestampCountById(bytes32 _queryId)
         external
         view
         returns (uint256);
 
-    function retrieveData(uint256 _requestId, uint256 _timestamp)
+    function getTimeOfLastNewValue() external view returns (uint256);
+
+    function getCurrentReward(bytes32 _queryId)
         external
         view
-        returns (uint256);
+        returns (uint256, uint256);
 
+    function getTipsById(bytes32 _queryId) external view returns (uint256);
+
+    function getTipsByUser(address _user) external view returns (uint256);
+
+    function tipsInContract() external view returns (uint256);
+}
+
+interface Master {
     function getAddressVars(bytes32 _data) external view returns (address);
 
-    function getRequestUintVars(uint256 _requestId, bytes32 _data)
-        external
-        view
-        returns (uint256);
+    function getUintVar(bytes32 _data) external view returns (uint256);
+}
+
+interface Governance {
+    function disputeFee() external view returns (uint256);
 }
 
 /**
  * @title Tellor Lens main contract
  * @dev Aggregate and simplify calls to the Tellor oracle.
  **/
-contract Main is UsingTellor {
+contract Main {
     Oracle public oracle;
+    Master public master;
+    Governance public governance;
 
     struct DataID {
-        uint256 id;
-        string name;
-        uint256 granularity;
+        bytes32 id;
     }
 
     struct Value {
         DataID meta;
         uint256 timestamp;
-        uint256 value;
         uint256 tip;
+        bytes value;
     }
 
-    address private admin;
-
-    DataID[] public dataIDs;
-    mapping(uint256 => uint256) public dataIDsMap;
-
-    constructor(address payable _oracle) UsingTellor(_oracle) {
+    constructor(
+        address payable _oracle,
+        address payable _master,
+        address payable _governance
+    ) {
         oracle = Oracle(_oracle);
-        admin = msg.sender;
-    }
-
-    modifier onlyAdmin {
-        require(msg.sender == admin, "not an admin");
-        _;
-    }
-
-    function setOracle(address _oracle) external onlyAdmin {
-        oracle = Oracle(_oracle);
-    }
-
-    function setAdmin(address _admin) external onlyAdmin {
-        admin = _admin;
-    }
-
-    function replaceDataIDs(DataID[] memory _dataIDs) external onlyAdmin {
-        delete dataIDs;
-        for (uint256 i = 0; i < _dataIDs.length; i++) {
-            dataIDs.push(_dataIDs[i]);
-            dataIDsMap[_dataIDs[i].id] = i;
-        }
-    }
-
-    function setDataID(uint256 _id, DataID memory _dataID) external onlyAdmin {
-        dataIDs[_id] = _dataID;
-        dataIDsMap[_dataID.id] = _id;
-    }
-
-    function pushDataID(DataID memory _dataID) external onlyAdmin {
-        dataIDs.push(_dataID);
-        dataIDsMap[_dataID.id] = dataIDs.length - 1;
-    }
-
-    function dataIDsAll() external view returns (DataID[] memory) {
-        return dataIDs;
+        master = Master(_master);
+        governance = Governance(_governance);
     }
 
     /**
+     * @param _queryId bytes32 hash of queryId.
      * @return Returns the current reward amount.
      */
-    function currentReward() external view returns (uint256) {
-        uint256 timeDiff =
-            block.timestamp -
-                oracle.getUintVar(keccak256("_TIME_OF_LAST_NEW_VALUE"));
-        uint256 rewardAmount = 1e18;
-
-        uint256 rewardAccumulated = (timeDiff * rewardAmount) / 300; // 1TRB every 6 minutes.
-
-        uint256 tip = oracle.getUintVar(keccak256("_CURRENT_TOTAL_TIPS")) / 10; // Half of the tips are burnt.
-        return rewardAccumulated + tip;
+    function getCurrentReward(bytes32 _queryId)
+        external
+        view
+        returns (uint256, uint256)
+    {
+        return oracle.getCurrentReward(_queryId);
     }
 
     /**
-     * @param _dataID is the ID for which the function returns the values for. When dataID is negative it returns the values for all dataIDs.
+     * @param _queryId is the ID for which the function returns the values for.
      * @param _count is the number of last values to return.
      * @return Returns the last N values for a request ID.
      */
-    function getLastValues(uint256 _dataID, uint256 _count)
+    function getLastValues(bytes32 _queryId, uint256 _count)
         public
         view
         returns (Value[] memory)
     {
-        uint256 totalCount = oracle.getNewValueCountbyRequestId(_dataID);
+        uint256 totalCount = oracle.getTimestampCountById(_queryId); //replaced
         if (_count > totalCount) {
             _count = totalCount;
         }
         Value[] memory values = new Value[](_count);
         for (uint256 i = 0; i < _count; i++) {
-            uint256 ts =
-                oracle.getTimestampbyRequestIDandIndex(
-                    _dataID,
-                    totalCount - i - 1
-                );
-            uint256 v = oracle.retrieveData(_dataID, ts);
+            uint256 ts = oracle.getReportTimestampByIndex( //replaced
+                _queryId,
+                totalCount - i - 1
+            );
+            bytes memory v = oracle.getValueByTimestamp(_queryId, ts); //replaced
             values[i] = Value({
-                meta: DataID({
-                    id: _dataID,
-                    name: dataIDs[dataIDsMap[_dataID]].name,
-                    granularity: dataIDs[dataIDsMap[_dataID]].granularity
-                }),
+                meta: DataID({id: _queryId}),
                 timestamp: ts,
                 value: v,
-                tip: totalTip(_dataID)
+                tip: oracle.getTipsById(_queryId) //replaced
             });
         }
 
@@ -150,18 +121,19 @@ contract Main is UsingTellor {
     }
 
     /**
-     * @param count is the number of last values to return.
-     * @return Returns the last N values for a data IDs.
+     * @param _count is the number of last values to return.
+     * @param _queryIds is a bytes32 array of queryIds.
+     * @return Returns the last N values for a specified queryIds.
      */
-    function getLastValuesAll(uint256 count)
+    function getLastValuesAll(uint256 _count, bytes32[] memory _queryIds)
         external
         view
         returns (Value[] memory)
     {
-        Value[] memory values = new Value[](count * dataIDs.length);
+        Value[] memory values = new Value[](_count * _queryIds.length);
         uint256 pos = 0;
-        for (uint256 i = 0; i < dataIDs.length; i++) {
-            Value[] memory v = getLastValues(dataIDs[i].id, count);
+        for (uint256 i = 0; i < _queryIds.length; i++) {
+            Value[] memory v = getLastValues(_queryIds[i], _count);
             for (uint256 ii = 0; ii < v.length; ii++) {
                 values[pos] = v[ii];
                 pos++;
@@ -174,117 +146,72 @@ contract Main is UsingTellor {
      * @return Returns the contract deity that can do things at will.
      */
     function deity() external view returns (address) {
-        return oracle.getAddressVars(keccak256("_DEITY"));
+        return master.getAddressVars(keccak256("_DEITY"));
     }
 
     /**
      * @return Returns the contract owner address.
      */
     function owner() external view returns (address) {
-        return oracle.getAddressVars(keccak256("_OWNER"));
+        return master.getAddressVars(keccak256("_OWNER"));
     }
 
     /**
      * @return Returns the contract pending owner.
      */
     function pendingOwner() external view returns (address) {
-        return oracle.getAddressVars(keccak256("_PENDING_OWNER"));
+        return master.getAddressVars(keccak256("_PENDING_OWNER"));
     }
 
     /**
      * @return Returns the contract address that executes all proxy calls.
      */
     function tellorContract() external view returns (address) {
-        return oracle.getAddressVars(keccak256("_TELLOR_CONTRACT"));
+        return master.getAddressVars(keccak256("_TELLOR_CONTRACT"));
     }
 
     /**
-     * @param _dataID is the ID for which the function returns the total tips.
-     * @return Returns the current tips for a give request ID.
+     * @param _queryId is the ID for which the function returns the total tips.
+     * @return Returns the current tips for a give query ID.
      */
-    function totalTip(uint256 _dataID) public view returns (uint256) {
-        return oracle.getRequestUintVars(_dataID, keccak256("_TOTAL_TIP"));
+    function totalTip(bytes32 _queryId) public view returns (uint256) {
+        return oracle.getTipsById(_queryId);
     }
 
     /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the last time when a value was submitted.
+     * @return Returns the last time a value was submitted by a reporter.
      */
     function timeOfLastValue() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_TIME_OF_LAST_NEW_VALUE"));
+        return oracle.getTimeOfLastNewValue();
     }
 
     /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the total number of requests from user thorugh the addTip function.
+     * @param _user address of the user we want to find out totalTip amount.
+     * @return Returns the total number of tips from a user.
      */
-    function requestCount() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_REQUEST_COUNT"));
+    function totalTipsByUser(address _user) external view returns (uint256) {
+        return oracle.getTipsByUser(_user);
     }
 
     /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the total oracle blocks.
+     * @return Returns the total amount of tips in the Oracle contract.
      */
-    function tBlock() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_T_BLOCK"));
+    function tipsInContract() external view returns (uint256) {
+        return oracle.tipsInContract();
     }
 
     /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the current block difficulty.
-     *
-     */
-    function difficulty() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_DIFFICULTY"));
-    }
-
-    /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable is used to calculate the block difficulty based on
-     * the time diff since the last oracle block.
-     */
-    function timeTarget() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_TIME_TARGET"));
-    }
-
-    /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the highest api/timestamp PayoutPool.
-     */
-    function currentTotalTips() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_CURRENT_TOTAL_TIPS"));
-    }
-
-    /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the number of miners who have mined this value so far.
-     */
-    function slotProgress() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_SLOT_PROGRESS"));
-    }
-
-    /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks the cost to dispute a mined value.
+     * @return Returns the current dispute fee amount.
      */
     function disputeFee() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_DISPUTE_FEE"));
+        return governance.disputeFee();
     }
 
     /**
-     * @return Returns the getUintVar variable named after the function name.
-     */
-    function disputeCount() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_DISPUTE_COUNT"));
-    }
-
-    /**
-     * @return Returns the getUintVar variable named after the function name.
-     * This variable tracks stake amount required to become a miner.
+     * @return Returns a variable that tracks the stake amount required to become a reporter.
      */
     function stakeAmount() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_STAKE_AMOUNT"));
+        return master.getUintVar(keccak256("_STAKE_AMOUNT"));
     }
 
     /**
@@ -292,6 +219,6 @@ contract Main is UsingTellor {
      * This variable tracks the number of parties currently staked.
      */
     function stakeCount() external view returns (uint256) {
-        return oracle.getUintVar(keccak256("_STAKE_AMOUNT"));
+        return master.getUintVar(keccak256("_STAKE_AMOUNT"));
     }
 }
